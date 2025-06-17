a35395ad289951ed02fc66691a44749ab6b3f931
Purnima Basnet <purnimabasnet@192.168.1.34>
2025-06-17 16:33:03 +0545

    sets and its functions

---
+++ set.py
@@ -0,0 +1,23 @@
+# #sets in python
+
+set1={"hello","python" , 2, 99, 5.4,"hello",2}
+set2 = {"hello", 6,3.4,"text",99}
+# #print(set1)
+
+
+# #adding data to set
+# set1.add(551)
+# #print(set1)
+
+# #union in set
+# print("union: ",set1.union(set2))
+
+# #intersection in set
+# print("intersection: ",set1.intersection(set2))
+#print("intersection: ",set1 & set2)
+# #difference in set
+# print("difference: ", set1.difference(set2))
+# print("difference: ",set1 -set2)
+
+#symmetric differences
+print("symmetric difference :", set1.symmetric_difference(set2))
