b8efd52c00a6e839d0419181a81c2e07bab493cc
Purnima Basnet <purnimabasnet@192.168.1.2>
2025-06-18 16:59:12 +0545

    aceesing class

---
+++ accessing_classes.py
@@ -0,0 +1,10 @@
+#importing classes and function
+from encapsulation import vehicle 
+
+class Car(vehicle):
+    def isplayCarDetails():
+        print("This is car Details:")
+        
+car = Car
+car.displayDetails()
+print(car.displayDetails())
