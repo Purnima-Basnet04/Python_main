89e121d122e0bbc4e88b36ae1ff98ff5d1e607cc
Purnima Basnet <purnimabasnet@192.168.1.2>
2025-06-18 16:33:23 +0545

    Classes in python

---
+++ classes_inheritance.py
@@ -0,0 +1,24 @@
+# Class in python
+class A:
+    def displayClassA():
+        print("Hello User !")
+        
+# Inheritance in python
+class B(A):
+    def displayClassB():
+        print("this is class B")
+        
+class C(B):
+    def displayClassC():
+        print("this is class C")
+        
+    # creating an instance of class A    
+obj = A
+#obj.displayClassA()
+obj2= B
+# obj2.displayClassB()# output: This is class B
+# obj2.displayClassA()# output: This is class A inherited by class B
+obj3= C
+obj3.displayClassC()
+obj3.displayClassB()
+obj3.displayClassA()
