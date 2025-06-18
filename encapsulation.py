f8bf513fba37232d7f417e4aee2b731c84a7635b
Purnima Basnet <purnimabasnet@192.168.1.2>
2025-06-18 16:51:05 +0545

    Encapsulation

---
+++ encapsulation.py
@@ -0,0 +1,25 @@
+class Vehicle:
+    #public function
+    def displayName():
+        return "Ford"
+    
+#protected function
+    def _displayModel():
+        return "Mustang"
+
+#private function
+    def _engine():
+        return "V8"
+
+# public function to display private and protected data
+    def displayDetails():
+        name= Vehicle.displayName()
+        model= Vehicle._displayModel()
+        engine= Vehicle._engine()
+        return f"Name:{ name}, Model:{model}, Engine: {engine}"
+
+
+vehicle= Vehicle
+print(vehicle.displayName())# accessing public function
+print(vehicle._displayModel())# accessing protected function
+print(vehicle.displayDetails())# accessing public function to display priovate and protected data
